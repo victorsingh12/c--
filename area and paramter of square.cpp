3403fd0215a329e8b0330bca159e866fab9df0d5
victorsingh12 <victorsinghsoftdev@gmail.com>
2024-02-28 10:50:37 +0530

    area and paramter of square

---
+++ area and paramter of square.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int side,area,parameter;
+    
+    cout<<"Enter Side  ";
+    cin >>side;
+    
+    
+    area=side*side;
+    parameter=4*side;
+    
+    cout<<"Area Of Sqaure Is "<<area;
+    cout<<"\nParameter Of Sqaure Is "<<parameter;
+    
+    return 0;
+    
+}
