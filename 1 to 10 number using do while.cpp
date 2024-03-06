a38822e266f9237a1e9f7286b5c8242de451357f
victorsingh12 <victorsinghsoftdev@gmail.com>
2024-03-06 10:58:27 +0530

    1 to 10 number using do while

---
+++ 1 to 10 number using do while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=10,i=1;
+    
+    do{
+        cout<<i<<endl;
+        i++;
+        
+    }while(i<=num);
+    return 0;
+    
+}
