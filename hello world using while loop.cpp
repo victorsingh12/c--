2567ae41ca74f6c00293b6b46e4b3eeeb6195b0b
victorsingh12 <victorsinghsoftdev@gmail.com>
2024-03-06 10:00:32 +0530

    hello world using while loop

---
+++ hello world using while loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=5,i=0;
+    
+   while(i<num)
+   {
+       cout<<"Hello World\n"<<endl;
+       i++;
+       
+   }
+    return 0;
+    
+}
